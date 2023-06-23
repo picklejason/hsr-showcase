76b7d6f3e62d29484fcd6361560c39e10be79b36
Jason Chen <jasonchen0429@gmail.com>
2023-06-23 12:07:58 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, session
+import requests
+from maps import mapped, substats, main_stats
+
+app = Flask(__name__)
+# app.config["TEMPLATES_AUTO_RELOAD"] = True
+asset_url = "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/"
+
+
+class DataStore:
+    a = None
+
+
+d = DataStore()
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/profile", methods=["GET"])
+def profile():
+    uid = request.args.get("uid")
+    url = f"https://api.mihomo.me/sr_info_parsed/{uid}?lang=en&version=v1"
+    res = requests.get(url)
+    data = res.json()
+    d.a = data
+
+    return render_template("profile.html", data=data, asset_url=asset_url)
+
+
+@app.route("/character")
+def character():
+    uid = d.a["player"]["uid"]
+    data = d.a["characters"]
+    chara = int(request.args.get("chara"))
+
+    return render_template(
+        "character.html",
+        uid=uid,
+        data=data,
+        chara=chara,
+        asset_url=asset_url,
+    )
+
+
+if __name__ == "__main__":
+    app.run()
