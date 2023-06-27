7ebc501d9793158d6e316f8ac9285c7ce56055c6
Jason Chen <jasonchen0429@gmail.com>
2023-06-27 01:48:53 -0400

    reduce cache timeout

--- api/app.py
+++ api/app.py
@@ -11,7 +11,7 @@ cache = Cache(
     app,
     config={
         "CACHE_TYPE": "SimpleCache",
-        "CACHE_DEFAULT_TIMEOUT": 600,
+        "CACHE_DEFAULT_TIMEOUT": 300,
         "CACHE_THRESHOLD": 1000,
     },
 )
