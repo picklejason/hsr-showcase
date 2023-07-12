799cebbdfee523bd32776ca81705f944f5bd4395
Jason Chen <jasonchen0429@gmail.com>
2023-07-12 13:35:47 -0400

    update input

--- app/Search.jsx
+++ app/Search.jsx
@@ -8,11 +8,13 @@ export default function Search() {
 
   return (
     <div className="flex flex-col items-center gap-3 text-black text-centers">
+      <label htmlFor="uid" className="sr-only">Enter UID</label>
       <input
         type="text"
         name="uid"
         onChange={(e) => setUID(e.target.value)}
         value={UID}
+        placeholder="Enter UID"
         className="text-center bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
       />
       <div className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded w-1/2">
