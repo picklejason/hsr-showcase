cfcf174e04c15ce6f4a6ed8a2e0c1e67a9e06c1a
Jason Chen <jasonchen0429@gmail.com>
2023-07-12 16:37:07 -0400

    add analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: "HSR Showcase",
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
