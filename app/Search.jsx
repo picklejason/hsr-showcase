a4caab166da64eb6d5d8a5b211f1de9d37948a4f
Jason Chen <jasonchen0429@gmail.com>
2023-07-24 02:04:48 -0400

    save uid local storage

--- app/Search.jsx
+++ app/Search.jsx
@@ -1,13 +1,25 @@
 'use client';
 
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 export default function Search() {
   const [UID, setUID] = useState('');
+  const [savedUID, setSavedUID] = useState('');
+
+  useEffect(() => {
+    if (UID.length === 9) {
+      localStorage.setItem('uid', UID);
+    }
+  }, [UID]);
+
+  useEffect(() => {
+    setSavedUID(localStorage.getItem('uid'));
+  }, []);
 
   return (
-    <div className="text-centers flex flex-col items-center gap-3 text-black">
+    <div className="text-centers flex flex-col items-center gap-3">
       <label htmlFor="uid" className="sr-only">
         Enter UID
       </label>
@@ -22,6 +34,19 @@ export default function Search() {
       <div className="focus:shadow-outline w-1/2 rounded bg-purple-600 px-4 py-2 font-bold text-white shadow hover:bg-purple-500 focus:outline-none">
         <Link href={`/u/${UID}`}>Search</Link>
       </div>
+      {savedUID && (
+        <div className="flex cursor-pointer flex-row items-center space-x-1 rounded-full bg-stone-800 px-3 py-1 shadow-md shadow-stone-900 hover:brightness-110 active:shadow-none">
+          <a href={`/u/${savedUID}`} className="flex gap-2">
+            <Image
+              src="https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/sign/SettingsAccount.png"
+              alt="Example Icon"
+              width={24}
+              height={24}
+            />
+            <span>Linked Profile: {localStorage.getItem('uid')}</span>
+          </a>
+        </div>
+      )}
     </div>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,10 @@
 import Search from './Search';
-import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center gap-3 space-y-2 p-12 text-center">
       <span className="text-6xl">Trailblazer Profile</span>
       <Search />
-      <div className="flex cursor-pointer flex-row items-center space-x-1 rounded-full bg-stone-800 px-3 py-1 shadow-md shadow-stone-900 hover:brightness-110 active:shadow-none">
-        <a href="/u/600516595" className="flex gap-2">
-          <Image
-            src="https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/icon/sign/SettingsAccount.png"
-            alt="Example Icon"
-            width={24}
-            height={24}
-          />
-          <span>Example</span>
-        </a>
-      </div>
     </div>
   );
 }
