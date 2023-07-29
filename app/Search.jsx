18f829e638d32b1e2ebdf9c71aa0366b98176bd2
Jason Chen <jasonchen0429@gmail.com>
2023-07-29 16:30:29 -0400

    layout

--- app/Search.jsx
+++ app/Search.jsx
@@ -17,16 +17,18 @@ export default function Search() {
       <label htmlFor="uid" className="sr-only">
         Enter UID
       </label>
-      <input
-        type="text"
-        name="uid"
-        onChange={(e) => setUID(e.target.value)}
-        value={UID}
-        placeholder="Enter UID"
-        className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 text-center leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
-      />
-      <div className="focus:shadow-outline w-1/2 rounded bg-purple-600 px-4 py-2 font-bold text-white shadow hover:bg-purple-500 focus:outline-none">
-        <Link href={`/u/${UID}`}>Search</Link>
+      <div className="flex flex-row gap-3">
+        <input
+          type="text"
+          name="uid"
+          onChange={(e) => setUID(e.target.value)}
+          value={UID}
+          placeholder="Enter UID"
+          className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 text-center leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+        />
+        <div className="focus:shadow-outline cursor-pointer rounded bg-purple-600 px-4 py-2 font-bold text-white shadow hover:bg-purple-500 focus:outline-none">
+          <Link href={`/u/${UID}`}>Search</Link>
+        </div>
       </div>
       {savedUID && (
         <div className="flex cursor-pointer flex-row items-center space-x-1 rounded bg-stone-800 px-3 py-1 shadow-md shadow-stone-900 hover:brightness-110 active:shadow-none">
--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,10 @@ import Search from './Search';
 export default function Home() {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center gap-3 space-y-2 p-12 text-center">
-      <span className="text-6xl">Trailblazer Profile</span>
+      <span className="text-6xl">HSR Showcase</span>
+      <span className="text-xl">
+        Fetch data from your Trailblazer Profile and display a build card for Honkai: Star Rail
+      </span>
       <Search />
     </div>
   );
