c6fc645f00e1ea1f5527c517fc7348be4c885429
Jason Chen <jasonchen0429@gmail.com>
2023-08-06 00:47:07 -0400

    change to heading

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Search from './Search';
 export default function Home() {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center gap-3 space-y-2 p-12 text-center">
-      <span className="text-6xl">HSR Showcase</span>
+      <h1 className="text-6xl">HSR Showcase</h1>
       <span className="text-xl">
         Fetch data from your Trailblazer Profile and display a build card for Honkai: Star Rail
       </span>
