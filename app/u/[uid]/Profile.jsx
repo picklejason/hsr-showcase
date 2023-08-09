d0f80cfd1957df1eccc686111b35bed8b6f629b1
Jason Chen <jasonchen0429@gmail.com>
2023-08-09 18:55:24 -0400

    revert scroll

--- app/u/[uid]/Profile.jsx
+++ app/u/[uid]/Profile.jsx
@@ -286,10 +286,8 @@ const Profile = () => {
             </div>
             {character && (
               <>
-                <div className="w-screen overflow-x-auto">
-                  <div className="showcase mx-3" ref={ref} style={{ fontFamily: 'var(--font-nunito_sans)' }}>
-                    <CharacterCard character={character} uid={uid} nickname={nickname} showUID={showUID} blur={blur} />
-                  </div>
+                <div className="showcase mx-3" ref={ref} style={{ fontFamily: 'var(--font-nunito_sans)' }}>
+                  <CharacterCard character={character} uid={uid} nickname={nickname} showUID={showUID} blur={blur} />
                 </div>
 
                 <div className="flex w-screen flex-col items-center justify-center">
