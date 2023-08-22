81a5a07edb32f9cee6d99d1c2b2f175229855917
Jason Chen <jasonchen0429@gmail.com>
2023-08-22 04:14:28 -0400

    ko-fi

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -1,13 +1,26 @@
 'use client';
 
 import { AiOutlineGithub } from 'react-icons/ai';
+import Script from 'next/script';
 
 const Footer = () => {
   return (
     <footer className="mx-auto max-w-3xl overflow-hidden px-4 sm:px-6 md:max-w-5xl">
+      <Script
+        src="https://storage.ko-fi.com/cdn/scripts/overlay-widget.js"
+        onLoad={() =>
+          kofiWidgetOverlay.draw('picklejason', {
+            type: 'floating-chat',
+            'floating-chat.donateButton.text': 'Support me',
+            'floating-chat.donateButton.background-color': '#292524',
+            'floating-chat.donateButton.text-color': '#fff',
+          })
+        }
+      />
       <hr className="mx-auto mt-8 h-0.5 w-full border-0 bg-white"></hr>
       <div className="mx-auto flex flex-row items-center gap-1 p-4 text-center md:flex-row md:justify-between">
         <span>© 2023 Jason</span>
+
         <span className="text-sm">
           This site is not affiliated with miHoYo &amp; All game content and assets are trademarks and copyrights of
           miHoYo.
