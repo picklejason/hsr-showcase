b7c67ea17d671f9870cfdf5adba17ad13105cf6b
Jason Chen <jasonchen0429@gmail.com>
2023-09-04 18:34:13 -0400

    loading text

--- app/u/[uid]/loading.js
+++ app/u/[uid]/loading.js
@@ -3,8 +3,9 @@ import Image from 'next/image';
 export default function Loading() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <div className="flex">
+      <div className="flex flex-col items-center gap-3">
         <Image src="/herta-kurukuru.gif" alt="Loading" width={256} height={256} />
+        <p className="text-4xl">Loading...</p>
       </div>
     </div>
   );
