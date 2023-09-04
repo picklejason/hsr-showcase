30a964098c78a892329aa646144c06f28abaae54
Jason Chen <jasonchen0429@gmail.com>
2023-09-04 18:24:33 -0400

    update footer

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -2,36 +2,29 @@
 
 import { AiOutlineGithub } from 'react-icons/ai';
 import { BsDiscord } from 'react-icons/bs';
-import Script from 'next/script';
+import { SiKofi } from 'react-icons/si';
 
 const Footer = () => {
   return (
     <footer className="mx-auto max-w-3xl overflow-hidden px-4 sm:px-6 md:max-w-5xl">
-      <Script
-        src="https://storage.ko-fi.com/cdn/scripts/overlay-widget.js"
-        onLoad={() =>
-          kofiWidgetOverlay.draw('picklejason', {
-            type: 'floating-chat',
-            'floating-chat.donateButton.text': 'Support me',
-            'floating-chat.donateButton.background-color': '#292524',
-            'floating-chat.donateButton.text-color': '#fff',
-          })
-        }
-      />
       <hr className="mx-auto mt-8 h-0.5 w-full border-0 bg-white"></hr>
-      <div className="mx-auto flex flex-row items-center gap-1 p-4 text-center md:flex-row md:justify-between">
+      <div className="mx-auto flex flex-col items-center gap-1 p-4 text-center md:flex-row md:justify-between">
         <span>© 2023 Jason</span>
-
         <span className="text-sm">
           This site is not affiliated with miHoYo &amp; All game content and assets are trademarks and copyrights of
           miHoYo.
         </span>
-        <a href="https://discord.gg/XZxuYuhDTa" aria-label="Discord" rel="noreferrer" target="_blank">
-          <BsDiscord className="cursor-pointer transition-transform hover:-translate-y-1" size={30} />
-        </a>
-        <a href="https://github.com/picklejason/starrail" aria-label="GitHub" rel="noreferrer" target="_blank">
-          <AiOutlineGithub className="cursor-pointer transition-transform hover:-translate-y-1" size={30} />
-        </a>
+        <div className="mb-1 flex flex-row items-center justify-center space-x-2">
+          <a href="https://discord.gg/XZxuYuhDTa" aria-label="Discord" rel="noreferrer" target="_blank">
+            <BsDiscord className="cursor-pointer transition-transform hover:-translate-y-1" size={30} />
+          </a>
+          <a href="https://github.com/picklejason/starrail" aria-label="GitHub" rel="noreferrer" target="_blank">
+            <AiOutlineGithub className="cursor-pointer transition-transform hover:-translate-y-1" size={30} />
+          </a>
+          <a href="https://ko-fi.com/picklejason" aria-label="Kofi" rel="noreferrer" target="_blank">
+            <SiKofi className="cursor-pointer transition-transform hover:-translate-y-1" size={30} />
+          </a>
+        </div>
       </div>
     </footer>
   );
