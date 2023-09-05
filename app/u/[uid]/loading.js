b992f0c601ec6e6f4493b2cb479bea4bc82d43c7
Jason Chen <jasonchen0429@gmail.com>
2023-09-04 21:39:39 -0400

    image priority

--- app/u/[uid]/loading.js
+++ app/u/[uid]/loading.js
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="flex flex-col items-center gap-3">
-        <Image src="/herta-kurukuru.gif" alt="Loading" width={256} height={256} />
+        <Image src="/herta-kurukuru.gif" alt="Loading" width={256} height={256} priority={true} />
         <p className="text-4xl">Loading...</p>
       </div>
     </div>
